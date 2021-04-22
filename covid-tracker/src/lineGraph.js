1e769d171099bf1002cdf9496d1ad1d06ccc97a4
reading-stiener <apradha1@conncoll.edu>
2021-04-22 09:13:23 -0400

    Attempted data pull on api endpoint

--- covid-tracker/src/lineGraph.js
+++ covid-tracker/src/lineGraph.js
@@ -1,2 +1,9 @@
-const { queryBQ } = require("./BigQueryClient");
-const data = queryBQ();
+console.log("script started");
+d3.json("https://api.covidtracking.com/v1/us/daily.json", (error, data) => {
+  if (error) { 
+    return console.warn(error)
+  }
+  console.log("hello");
+  console.log(data);
+})
+console.log("script ended");
