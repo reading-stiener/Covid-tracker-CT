0ed7ea788b485a4d27756048d73d7018260aa8eb
reading-stiener <apradha1@conncoll.edu>
2021-04-22 23:17:10 -0400

    Set up linechart script

--- covid-tracker/src/appExpress.js
+++ covid-tracker/src/appExpress.js
@@ -21,89 +21,89 @@ app.get("/covidgendata", async (req, res) => {
         // const params = req.params; ß
         // console.log(params.city);
         const covidData = await queryBQ();
-        const testData = [
-            {
-                "province_state": "Connecticut",
-                "country_region": "United States of America",
-                "date": {
-                    "value": "2020-01-31"
-                },
-                "latitude": 41.6,
-                "longitude": -72.7,
-                "state": "Connecticut",
-                "county": null,
-                "location_geom": {
-                    "value": "POINT(-72.7 41.6)"
-                },
-                "confirmed": 0,
-                "deaths": 0,
-                "recovered": null,
-                "active": null,
-                "fips": null,
-                "combined_key": "US_CT"
-            },
-            {
-                "province_state": "Connecticut",
-                "country_region": "United States of America",
-                "date": {
-                    "value": "2020-02-20"
-                },
-                "latitude": 41.6,
-                "longitude": -72.7,
-                "state": "Connecticut",
-                "county": null,
-                "location_geom": {
-                    "value": "POINT(-72.7 41.6)"
-                },
-                "confirmed": 0,
-                "deaths": 0,
-                "recovered": null,
-                "active": null,
-                "fips": null,
-                "combined_key": "US_CT"
-            },
-            {
-                "province_state": "Connecticut",
-                "country_region": "United States of America",
-                "date": {
-                    "value": "2021-04-16"
-                },
-                "latitude": 41.6,
-                "longitude": -72.7,
-                "state": "Connecticut",
-                "county": null,
-                "location_geom": {
-                    "value": "POINT(-72.7 41.6)"
-                },
-                "confirmed": 327956,
-                "deaths": 7986,
-                "recovered": null,
-                "active": null,
-                "fips": null,
-                "combined_key": "US_CT"
-            },
-            {
-                "province_state": "Connecticut",
-                "country_region": "United States of America",
-                "date": {
-                    "value": "2020-03-20"
-                },
-                "latitude": 41.6,
-                "longitude": -72.7,
-                "state": "Connecticut",
-                "county": null,
-                "location_geom": {
-                    "value": "POINT(-72.7 41.6)"
-                },
-                "confirmed": 194,
-                "deaths": 3,
-                "recovered": null,
-                "active": null,
-                "fips": null,
-                "combined_key": "US_CT"
-            }
-        ]
-        res.json(testData);
+        // const testData = [
+        //     {
+        //         "province_state": "Connecticut",
+        //         "country_region": "United States of America",
+        //         "date": {
+        //             "value": "2020-01-31"
+        //         },
+        //         "latitude": 41.6,
+        //         "longitude": -72.7,
+        //         "state": "Connecticut",
+        //         "county": null,
+        //         "location_geom": {
+        //             "value": "POINT(-72.7 41.6)"
+        //         },
+        //         "confirmed": 0,
+        //         "deaths": 0,
+        //         "recovered": null,
+        //         "active": null,
+        //         "fips": null,
+        //         "combined_key": "US_CT"
+        //     },
+        //     {
+        //         "province_state": "Connecticut",
+        //         "country_region": "United States of America",
+        //         "date": {
+        //             "value": "2020-02-20"
+        //         },
+        //         "latitude": 41.6,
+        //         "longitude": -72.7,
+        //         "state": "Connecticut",
+        //         "county": null,
+        //         "location_geom": {
+        //             "value": "POINT(-72.7 41.6)"
+        //         },
+        //         "confirmed": 0,
+        //         "deaths": 0,
+        //         "recovered": null,
+        //         "active": null,
+        //         "fips": null,
+        //         "combined_key": "US_CT"
+        //     },
+        //     {
+        //         "province_state": "Connecticut",
+        //         "country_region": "United States of America",
+        //         "date": {
+        //             "value": "2021-04-16"
+        //         },
+        //         "latitude": 41.6,
+        //         "longitude": -72.7,
+        //         "state": "Connecticut",
+        //         "county": null,
+        //         "location_geom": {
+        //             "value": "POINT(-72.7 41.6)"
+        //         },
+        //         "confirmed": 327956,
+        //         "deaths": 7986,
+        //         "recovered": null,
+        //         "active": null,
+        //         "fips": null,
+        //         "combined_key": "US_CT"
+        //     },
+        //     {
+        //         "province_state": "Connecticut",
+        //         "country_region": "United States of America",
+        //         "date": {
+        //             "value": "2020-03-20"
+        //         },
+        //         "latitude": 41.6,
+        //         "longitude": -72.7,
+        //         "state": "Connecticut",
+        //         "county": null,
+        //         "location_geom": {
+        //             "value": "POINT(-72.7 41.6)"
+        //         },
+        //         "confirmed": 194,
+        //         "deaths": 3,
+        //         "recovered": null,
+        //         "active": null,
+        //         "fips": null,
+        //         "combined_key": "US_CT"
+        //     }
+        // ]
+        res.json(covidData);
     } catch (error) {
         console.log(error);
         res.status(500).json({ success: false, error: error });
--- covid-tracker/src/lineGraph.js
+++ covid-tracker/src/lineGraph.js
@@ -16,32 +16,32 @@ var svg = d3.select("#my_dataviz")
 fetch("http://localhost:3000/covidgendata")
   .then(data => data.json())
   .then(data => {
-    console.log(data);
-  })
-//   var x = d3.scaleTime()
-//     .domain(d3.extent(data, function(d) { return d.date; }))
-//     .range([ 0, width ]);
-//   svg.append("g")
-//     .attr("transform", "translate(0," + height + ")")
-//     .call(d3.axisBottom(x));
+  var x = d3.scaleTime()
+    .domain(d3.extent(data, function(d) { return d.deaths; }))
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
 
-//   // Add Y axis
-//   var y = d3.scaleLinear()
-//     .domain([0, d3.max(data, function(d) { return +d.value; })])
-//     .range([ height, 0 ]);
-//   svg.append("g")
-//     .call(d3.axisLeft(y));
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) { return d.confirmed; })])
+    .range([ height, 0 ]);
+  
+  
+    svg.append("g")
+    .call(d3.axisLeft(y));
 
-//   // Add the line
-//   svg.append("path")
-//     .datum(data)
-//     .attr("fill", "none")
-//     .attr("stroke", "steelblue")
-//     .attr("stroke-width", 1.5)
-//     .attr("d", d3.line()
-//       .x(function(d) { return x(d.date) })
-//       .y(function(d) { return y(d.confirmed) })
-//     ) 
-// })
+  // Add the line
+  svg.append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr("d", d3.line()
+      .x(function(d) { return x(d.deaths) })
+      .y(function(d) { return y(d.confirmed) })
+    ) 
+})
 //console.log(data);
 console.log("script ended");
